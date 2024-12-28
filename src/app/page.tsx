baa3883049f2081e4f4b821443a8a5b34c01e897
MarkC1-code <marksimon5093@gmail.com>
2024-12-28 21:15:32 +0100

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ const staticbet: React.FC = () => {
   
   return (
     <div className="bg-black text-white min-h-screen font-sans">
-      <div className="max-w-sm mx-auto p-4 -pr-2 ">
+      <div className="max-w-sm mx-auto p-4 -pr-2 pb-0 -mb-2 pt-0" >
         {/* Tab Navigation */}
         <Image src='/sportyheader.jpg' alt="head" width={1000} height={1000} className="sticky top-0" />
 
           <BetHistory />
 
-        <Image src='/sportyfooter.jpg' alt="ok" width={1000} height={1000} className="sticky mt-5 bottom-2" />
+        <Image src='/sportyfooter.jpg' alt="ok" width={1000} height={1000} className="sticky mt-5 bottom-0" />
       </div>
     </div>
   );
