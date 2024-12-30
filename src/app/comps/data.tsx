70e07260c0f16cd793882c0cb4c591833d505d1e
MarkC1-code <marksimon5093@gmail.com>
2024-12-30 06:34:14 +0100

    editted values

--- src/app/comps/data.tsx
+++ src/app/comps/data.tsx
@@ -2,23 +2,23 @@ const data = [
   
     {
         day: 8,
-        stake: '5,000',
-        return: '11,260',
+        stake: '5,000.00',
+        return: '11,260.12',
         Match1: 'chelsea Fc vs Arsenal Fc',
         Match2: 'Atlanta Vs Napoli',
         win: true
     },
       {
         day: 7,
-        stake: 10000,
-        return: '20,260',
+        stake: '10,000.00',
+        return: '20,260.63',
         Match1: 'Man u vs Wolves',
           Match2: 'Atlanta Vs Napoli',
         win: true
     },
     {
         day: 6,
-        stake: '1,400',
+        stake: '1,400.00',
         return: '0.00',
         Match1: 'Man u vs Wolves',
         Match2: 'Atlanta Vs Napoli',
@@ -26,7 +26,7 @@ const data = [
     },
      {
         day: 5,
-        stake: '9,000',
+        stake: '9,000.00',
         return: '0.00',
         Match1: 'Man u vs Wolves',
          Match2: 'Atlanta Vs Napoli',
@@ -34,23 +34,23 @@ const data = [
     },
     {
         day: 4,
-        stake: '100,000',
-        return: '201,060',
+        stake: '100,000.00',
+        return: '201,060.02',
         Match1: 'Man u vs Wolves',
         Match2: 'Atlanta Vs Napoli',
         win: true
     },
     {
         day: 3,
-        stake: '1,000',
-        return: '20,200',
+        stake: '1,000.00',
+        return: '20,200.17',
         Match1: 'Man u vs Wolves',
         Match2: 'Atlanta Vs Napoli',
         win: true
     },
     {
         day: 2,
-        stake: '500',
+        stake: '500.00',
         return: '0.00',
         Match1: 'Man u vs Wolves',
         Match2: 'Atlanta Vs Napoli',
@@ -58,7 +58,7 @@ const data = [
     },
     {
         day: 1,
-        stake: '100',
+        stake: '100.00',
         return: '0.00',
         Match1: 'Man u vs Wolves',
         Match2: 'Atlanta Vs Napoli',
