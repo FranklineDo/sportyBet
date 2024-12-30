69bf30f12c72dcfc4efd6c2d747a54c2394cea07
MarkC1-code <marksimon5093@gmail.com>
2024-12-30 23:02:06 +0100

    almost

--- src/app/comps/data.tsx
+++ src/app/comps/data.tsx
@@ -3,11 +3,10 @@ const data = [
     {
         day: '30',
         month:'Dec',
-        stake: '120.00',
-        return: '195.30',
-        Match1: 'chelsea Fc vs Arsenal Fc',
-        Match2: 'Atlanta Vs Napoli',
-        win: true
+        stake: '20.00',
+        return: '0.00',
+        Match1: 'Enppi Club v AL Ahly SC (Egy)',
+        win: false
     }
 ]
  
