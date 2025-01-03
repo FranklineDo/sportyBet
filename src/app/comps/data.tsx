a28b1336ca851e31b08e1f9671f9e3e7d648c0b5
MarkC1-code <marksimon5093@gmail.com>
2025-01-03 14:10:06 +0100

    almost

--- src/app/comps/data.tsx
+++ src/app/comps/data.tsx
@@ -19,6 +19,36 @@ const data = [
         Match3: 'Plymouth Argyle v Bristol City',
         win: true
     },
+    {
+        day: '01',
+        month:'',
+        stake: '7,500.00',
+        return: '17,518.41',
+        Match1: 'Brentford FC v Arsenal',
+        Match2: 'Burnley v Stoke',
+        Match3: 'Plymouth Argyle v Bristol City',
+        win: true
+    },
+    {
+        day: '01',
+        month:'',
+        stake: '7,500.00',
+        return: '17,518.41',
+        Match1: 'Brentford FC v Arsenal',
+        Match2: 'Burnley v Stoke',
+        Match3: 'Plymouth Argyle v Bristol City',
+        win: true
+    },
+    {
+        day: '01',
+        month:'',
+        stake: '7,500.00',
+        return: '17,518.41',
+        Match1: 'Brentford FC v Arsenal',
+        Match2: 'Burnley v Stoke',
+        Match3: 'Plymouth Argyle v Bristol City',
+        win: true
+    },
 
 ]
  
