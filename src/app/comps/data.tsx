f569f345b7ded887923e3c9c7ddce13b2f866fc4
MarkC1-code <marksimon5093@gmail.com>
2025-01-04 12:54:06 +0100

    almost

--- src/app/comps/data.tsx
+++ src/app/comps/data.tsx
@@ -1,5 +1,15 @@
 const data = [
         {
+        day: '03',
+        month:'',
+        stake: '12,000.00',
+        return: '29,042.00',
+        Match1: 'Valecia v Real Madrid',
+        Match2: 'Juventus FC v Milan',
+        Match3: 'Nacional de Madeira v Porto',
+        win: true
+    },
+        {
         day: '02',
         month:'',
         stake: '10,000.00',
@@ -7,36 +17,6 @@ const data = [
         Match1: 'Rangers v Celtic',
         Match2: 'Maccabi Haifa FC v Maccabi Bney ...',
         Match3: 'Inter v Atalanta',
-        win: true
-    },
-    {
-        day: '01',
-        month:'',
-        stake: '7,500.00',
-        return: '17,518.41',
-        Match1: 'Brentford FC v Arsenal',
-        Match2: 'Burnley v Stoke',
-        Match3: 'Plymouth Argyle v Bristol City',
-        win: true
-    },
-    {
-        day: '01',
-        month:'',
-        stake: '7,500.00',
-        return: '17,518.41',
-        Match1: 'Brentford FC v Arsenal',
-        Match2: 'Burnley v Stoke',
-        Match3: 'Plymouth Argyle v Bristol City',
-        win: true
-    },
-    {
-        day: '01',
-        month:'',
-        stake: '7,500.00',
-        return: '17,518.41',
-        Match1: 'Brentford FC v Arsenal',
-        Match2: 'Burnley v Stoke',
-        Match3: 'Plymouth Argyle v Bristol City',
         win: true
     },
     {
