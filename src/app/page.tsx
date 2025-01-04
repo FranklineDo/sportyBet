ae52d9ef1c1adb3a46629be90a80431d1877af14
MarkC1-code <marksimon5093@gmail.com>
2025-01-04 13:10:50 +0100

    almost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import BetHistory from "./comps/datacomp";
 const staticbet: React.FC = () => {
   
   return (
-      <div className="max-w-sm min-h-screen flex flex-col bg-customBg text-white  mx-auto p-4 pr-0 pl-0 pb-0  font-sans -mb-2 pt-0" >
+      <div className="max-w-sm min-h-screen flex flex-col bg-customBg text-white  mx-auto p-4 pr-1 pl-0 pb-0  font-sans -mb-2 pt-0" >
         {/* Tab Navigation */}
       <div className=" flex-1">
         <Image src='/urlmain.jpg' width={1000} alt="ok" height={1000} className="sticky bottom-0" />
